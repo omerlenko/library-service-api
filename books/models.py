ceb65196cef4b3229857a1712db95a968f7ff257
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-03-17 13:59:50 +0100

    fix: Fix typo in models.py

--- books/models.py
+++ books/models.py
@@ -5,13 +5,13 @@ class Book(models.Model):
 
     class Cover(models.TextChoices):
         HARD = "HARD", "Hard"
-        PUBLISHED = "SOFT", "Soft"
+        SOFT = "SOFT", "Soft"
 
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=100)
     cover = models.CharField(max_length=4, choices=Cover.choices, default=Cover.HARD)
     inventory = models.PositiveIntegerField()
-    daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
 
     class Meta:
         constraints = [
