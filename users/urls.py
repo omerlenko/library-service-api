535d3953ed8dcc9489e04efc00f7ffdd801d1631
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-03-17 21:01:15 +0100

    feat: Add CreateUserView and ManageUserView endpoints

--- users/urls.py
+++ users/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
-    TokenVerifyView,
 )
 
+from users.views import CreateUserView, ManageUserView
+
 urlpatterns = [
+    path("", CreateUserView.as_view(), name="create_user"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("me/", ManageUserView.as_view(), name="manage_user"),
 ]
 
 app_name = "users"
