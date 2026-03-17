ea3341559c8bdcfd8d1956f957d08ca828e8a8be
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-03-17 12:24:16 +0100

    feat: Register Book in admin panel

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "debug_toolbar",
     "rest_framework",
+    "books",
 ]
 
 MIDDLEWARE = [
--- library_service_api/urls.py
+++ library_service_api/urls.py
@@ -17,9 +17,9 @@ Including another URLconf
 
 from debug_toolbar.toolbar import debug_toolbar_urls
 
-# from django.contrib import admin
+from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ] + debug_toolbar_urls()
