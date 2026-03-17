110ee753c57c76bae451cf290db0a6d2ba5f52b5
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-03-17 18:17:29 +0100

    chore: Add docs endpoints

--- library_service_api/urls.py
+++ library_service_api/urls.py
@@ -19,8 +19,25 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/books/", include("books.urls", namespace="books")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # Optional UI:
+    path(
+        "api/docs/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/docs/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ] + debug_toolbar_urls()
