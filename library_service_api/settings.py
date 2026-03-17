fae6efb56ff8916c98a06999219d0a3eb574b08d
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-03-17 20:32:19 +0100

    chore: Set JWTAuthentication as DEFAULT_AUTHENTICATION_CLASSES

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -125,6 +125,9 @@ STATIC_URL = "static/"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
 }
 
 SPECTACULAR_SETTINGS = {
