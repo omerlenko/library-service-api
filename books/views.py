e920cab4cb7566ce38691f6fb6b997528c092358
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-03-18 14:32:33 +0100

    feat: Set BookViewSet permission class to IsAdminOrReadOnly

--- books/views.py
+++ books/views.py
@@ -2,6 +2,7 @@ from drf_spectacular.utils import extend_schema_view, extend_schema
 from rest_framework import viewsets
 
 from books.models import Book
+from books.permissions import IsAdminOrReadOnly
 from books.serializers import BookSerializer, BookListSerializer
 
 
@@ -43,6 +44,7 @@ from books.serializers import BookSerializer, BookListSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
     def get_serializer_class(self):
         if self.action in ("list",):
