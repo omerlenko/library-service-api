31ef6f552d160cc4b7171d5f74a6d20e6a53b3b0
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-03-19 12:57:22 +0100

    docs: Add BorrowingViewSet initial documentation

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,4 +1,6 @@
+from drf_spectacular.utils import extend_schema_view, extend_schema
 from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
 
 from borrowings.models import Borrowing
 from borrowings.serializers import (
@@ -7,6 +9,18 @@ from borrowings.serializers import (
 )
 
 
+@extend_schema_view(
+    list=extend_schema(
+        summary="List borrowings",
+        description="Return a list of borrowings available to the authenticated user.",
+        responses=BorrowingListSerializer(many=True),
+    ),
+    retrieve=extend_schema(
+        summary="Retrieve borrowing",
+        description="Return detailed information about a specific borrowing.",
+        responses=BorrowingDetailSerializer,
+    ),
+)
 class BorrowingViewSet(
     viewsets.GenericViewSet,
     mixins.ListModelMixin,
@@ -14,6 +28,7 @@ class BorrowingViewSet(
 ):
     queryset = Borrowing.objects.select_related("user", "book")
     serializer_class = BorrowingListSerializer
+    permission_classes = (IsAuthenticated,)
 
     def get_serializer_class(self):
         if self.action == "list":
