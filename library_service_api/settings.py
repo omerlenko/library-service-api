7f08eecedb4a21c5ff793939e1c8aeba30227685
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-03-23 19:25:58 +0100

    chore: Add TELEGRAM_CHAT_ID and TELEGRAM_BOT_TOKEN to settings

--- library_service_api/settings.py
+++ library_service_api/settings.py
@@ -10,8 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from datetime import timedelta
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,3 +161,6 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": True,
     "AUTH_HEADER_NAME": "HTTP_AUTHORIZE",
 }
+
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
