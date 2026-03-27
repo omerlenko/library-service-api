66f4edccc032c161d3653bfce438ca38444d5a29
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-03-27 19:56:00 +0100

    feat: Add success and cancel payment endpoints

--- payments/views.py
+++ payments/views.py
@@ -1,7 +1,12 @@
+import stripe.checkout
 from drf_spectacular.utils import extend_schema_view, extend_schema
-from rest_framework import viewsets, mixins
-from rest_framework.permissions import IsAuthenticated
-
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from stripe import InvalidRequestError
 from payments.models import Payment
 from payments.serializers import PaymentListSerializer, PaymentDetailSerializer
 
@@ -50,3 +55,48 @@ class PaymentViewSet(
             queryset = queryset.filter(borrowing__user=self.request.user)
 
         return queryset
+
+    @action(
+        methods=["GET"],
+        detail=False,
+        permission_classes=(AllowAny,),
+    )
+    def success(self, request, *args, **kwargs):
+        session_id = request.query_params.get("session_id")
+        if not session_id:
+            raise ValidationError({"session_id": "This query parameter is required."})
+
+        try:
+            session = stripe.checkout.Session.retrieve(session_id)
+        except InvalidRequestError:
+            raise ValidationError("Session_id query parameter is invalid.")
+
+        if session.payment_status == "paid":
+            payment = get_object_or_404(Payment, session_id=session.id)
+            payment.status = Payment.Status.PAID
+            payment.save()
+
+            return Response(
+                {
+                    "detail": "Payment confirmed successfully.",
+                    "payment_status": "PAID",
+                },
+                status=status.HTTP_200_OK,
+            )
+        return Response(
+            {"detail": "This payment hasn't been paid yet."},
+            status=status.HTTP_200_OK,
+        )
+
+    @action(
+        methods=["GET"],
+        detail=False,
+        permission_classes=(AllowAny,),
+    )
+    def cancel(self, request, *args, **kwargs):
+        return Response(
+            {
+                "detail": "You can still pay later, the payment link will be active for 24 hours after placing the order."
+            },
+            status=status.HTTP_200_OK,
+        )
