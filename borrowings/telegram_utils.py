c221598c9bc4b1e712acc7f5eb7cddfcbefa60ab
Oleksandr Merlenko <merlenko.business@gmail.com>
2026-03-31 14:00:40 +0200

    feat: Add build_payment_details_message helper, update build_borrowing_details_message helper

--- borrowings/telegram_utils.py
+++ borrowings/telegram_utils.py
@@ -1,14 +1,29 @@
 import requests
 from borrowings.models import Borrowing
 from django.conf import settings
+from payments.models import Payment
 
 
 def build_borrowing_details_message(borrowing: Borrowing) -> str:
     text = (
+        f"Borrowing id: {borrowing.id}\n"
         f"Book: {borrowing.book.title} by {borrowing.book.author}\n"
         f"User: {borrowing.user.email}\n"
         f"Borrow date: {borrowing.borrow_date}\n"
         f"Expected return: {borrowing.expected_return_date}"
+    )
+    return text
+
+
+def build_payment_details_message(payment: Payment) -> str:
+    borrowing = payment.borrowing
+
+    text = (
+        f"Payment id: {payment.id}\n"
+        f"Payment type: {payment.payment_type}\n"
+        f"Payment amount: ${payment.money_to_pay}\n"
+        f"Book: {borrowing.book.title} by {borrowing.book.author}\n"
+        f"User: {borrowing.user.email}\n"
     )
     return text
 
